b597c2b43a7216fb1daf4f7c7fa0b28248db3ca7
DESKTOP-HF3GHF6\sono8 <sono888.gamestudio@gmail.com>
2018-06-10 22:37:36 +0900

    almost completed

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;//追加しました
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,23 +7,33 @@ using UnityEngine.UI;
 //入力された2つの小数を計算するプログラムです
 public class Calculator : MonoBehaviour
 {
+    const int SECTIONS = 2;
     const int MAXdigit = 8;//最大桁数
+    const int PASTlimit = 10;
 
     [SerializeField]
-    int[] value1, value2;//2つの入力値
-    int digitCounter1, digitCounter2;//現在の桁数カウンター
-    int dotPosition1, dotPosition2;//小数点の位置
-    Operator operatorType;
-    bool onFirstValue;//value1の入力中かどうか
+    int[,] values;//2つの入力値
+    int[] digitCounters;//現在の桁数カウンター
+    int[] dotPositions;//小数点の位置
+    bool[] minus;
+    int valueCounter;
+    int operatorNo;
+
+    List<double> pastValues;//過去の結果保持用
+    int pastIndex;//過去の結果をさかのぼったときの位置
 
     Text valueText;
 
     // Use this for initialization
     void Start()
     {
-        value1 = new int[MAXdigit];
-        value2 = new int[MAXdigit];
-        operatorType = 0;
+        values = new int[SECTIONS, MAXdigit];
+        digitCounters = new int[SECTIONS];
+        dotPositions = new int[SECTIONS];
+        minus = new bool[SECTIONS];
+
+        pastValues = new List<double>();
+        pastIndex = 0;
 
         valueText = GetComponent<Text>();
 
@@ -35,236 +46,252 @@ public class Calculator : MonoBehaviour
 
     }
 
-    //何も入力がない状態を-1として初期化するメソッド
-    public void InitializeCalculation()
+    void InitializeArray(int index)
     {
-        value1[0] = 0;
-        value2[0] = 0;
-        for (int i = 1; i < MAXdigit; i++)
+        values[index, 0] = 0;
+        for (int i = 1; i < MAXdigit; i++)//何も入力がない状態を-1として初期化
         {
-            value1[i] = -1;
-            value2[i] = -1;
+            values[index, i] = -1;
         }
 
-        digitCounter1 = 0;
-        digitCounter2 = 0;
+        digitCounters[index] = 0;
+        dotPositions[index] = -1;//未設定であれば-1に
+        minus[index] = false;
+    }
 
-        dotPosition1 = -1;//未設定であれば-1に
-        dotPosition2 = -1;
+    //初期化メソッド(「C」に対応)
+    public void InitializeCalculation()
+    {
+        for (int i = 0; i < SECTIONS; i++)
+        {
+            InitializeArray(i);
+        }
 
-        operatorType = 0;
-        onFirstValue = true;
-
+        operatorNo = 0;
+        valueCounter = 0;
         valueText.text = "0";
     }
 
-    //配列から数値に変換するメソッド
-    double ArrayToValue(int[] valueArray, int digitCounter, int dotPosition)
+    void ShowValueText(int valueIndex)
     {
-        double value = 0;
-        for (int i = 0; i < digitCounter; i++)
+        string s = "";
+        if (minus[valueIndex])
         {
-            value = value * 10 + valueArray[i];
+            s += "-";
         }
 
-        if (dotPosition != -1)
+        for (int i = 0; i < digitCounters[valueIndex]; i++)
         {
-            value /= Mathf.Pow(10, digitCounter - dotPosition - 1);//小数点の位置で桁を補正する
+            if (i != 0 && i == dotPositions[valueIndex] + 1)
+            {
+                s += ".";
+            }
+            s += values[valueIndex, i].ToString();
+        }
+
+        valueText.text = s;
+    }
+
+    //配列から数値に変換するメソッド
+    double ArrayToValue(int index)
+    {
+        double value = 0;
+        for (int i = 0; i < digitCounters[index]; i++)
+        {
+            value = value * 10 + values[index, i];
+        }
+
+        if (dotPositions[index] != -1)
+        {
+            value /= Mathf.Pow(10,
+                digitCounters[index] - dotPositions[index] - 1);//小数点の位置で桁を補正する
+        }
+        if (minus[index])
+        {
+            value *= -1;
         }
         return value;
     }
 
-    int[] ValueTextToArray(ref int dotPosition, ref int digitCounter)
+    //引数のインデックスのvalueに割り当て
+    void ValueToArray(double value, int outIndex)
     {
-        int[] array = new int[MAXdigit];
-        array[0] = 0;
-        for (int i = 1; i < MAXdigit; i++)
-        {
-            array[i] = -1;
-        }
-        string text = valueText.text;
+        minus[outIndex] = value < 0;
+        value = Math.Abs(value);
 
-        double val;
-        if (!double.TryParse(text, out val)) return array;
+        int digitNumber = (int)Math.Floor(Math.Log10(value)) + 1;//最大桁位置を取得
+        int nowNumber;//現在桁の値
+        //値を割り当て
+        for (int i = 0; i < MAXdigit; i++)
+        {
+            if (digitNumber == 1)//1の位であれば小数点を設定
+            {
+                dotPositions[outIndex] = i;
+            }
+            nowNumber = (int)Math.Floor(value / Math.Pow(10, digitNumber - 1));//最大桁の値
+            values[outIndex, i] = nowNumber;
 
-        int index = text.IndexOf('.');
-        if (index > 0)//小数点が表示されているとき
-        {
-            dotPosition = index - 1;
-            text = text.Replace(".", "");
+            value -= nowNumber * Math.Pow(10, digitNumber - 1);//最大桁を1つ下げる
+            digitNumber--;
         }
-        int length = text.Length;
-        if (MAXdigit < length)
-        {
-            digitCounter = MAXdigit;
-        }
-        else
-        {
-            digitCounter = length;
-        }
-        Debug.Log(digitCounter);
 
-        for (int i = 0; i < digitCounter; i++)
+        //最小桁から見てゆき、小数点以下かつ0ならば未入力状態(-1)に
+        for (int i = MAXdigit - 1; i >= 0; i--)
         {
-            array[i] = text[i] - '0';
+            if (digitNumber < 0 && values[outIndex, i] == 0)
+            {
+                values[outIndex, i] = -1;
+                if (digitNumber == -1)
+                {
+                    dotPositions[outIndex] = -1;
+                }
+                digitNumber++;
+            }
+            else
+            {
+                digitCounters[outIndex] = i + 1;//桁位置を設定
+                break;
+            }
         }
-        return array;
+    }
+
+    void AddPastValue(double value)
+    {
+        pastValues.Add(value);
+        if (pastValues.Count > PASTlimit)
+        {
+            pastValues.RemoveAt(0);
+        }
+        pastIndex = pastValues.Count;
     }
 
     //数字ボタンを押したとき
     public void InputNumber(int number)
     {
-        if (onFirstValue)
-        {
-            if (digitCounter1 >= MAXdigit
-                || (digitCounter1 == 0 && number == 0)) return;
-            value1[digitCounter1] = number;
+        if (digitCounters[valueCounter] >= MAXdigit
+            || (digitCounters[valueCounter] == 0 && number == 0)) return;
 
-            digitCounter1++;
-            valueText.text = ArrayToValue(value1, digitCounter1, dotPosition1).ToString("G");
-        }
-        else
-        {
-            if (digitCounter2 >= MAXdigit
-                || (digitCounter2 == 0 && number == 0)) return;
-            value2[digitCounter2] = number;
-
-            digitCounter2++;
-            valueText.text = ArrayToValue(value2, digitCounter2, dotPosition2).ToString("G");
-        }
+        values[valueCounter, digitCounters[valueCounter]] = number;
+        digitCounters[valueCounter]++;
+        ShowValueText(valueCounter);
     }
 
     public void DeleteNumber()
     {
-        if (onFirstValue)
-        {
-            if (digitCounter1 == 0) return;//何も入力されていなければ中断
-            value1[digitCounter1] = -1;
-            digitCounter1--;
-            if (digitCounter1 < dotPosition1)
-            {
-                dotPosition1 = -1;
-            }
+        if (digitCounters[valueCounter] == 0) return;
 
-            valueText.text = ArrayToValue(value1, digitCounter1, dotPosition1).ToString("G");
-        }
-        else
+        values[valueCounter, digitCounters[valueCounter]] = -1;
+        digitCounters[valueCounter]--;
+        if (digitCounters[valueCounter] < dotPositions[valueCounter])
         {
-            if (digitCounter2 == 0) return;//何も入力されていなければ中断
-            value2[digitCounter2] = -1;
-            digitCounter2--;
-            if (digitCounter2 < dotPosition2)
-            {
-                dotPosition2 = -1;
-            }
-
-            valueText.text = ArrayToValue(value2, digitCounter2, dotPosition2).ToString("G");
+            dotPositions[valueCounter] = -1;
         }
+        ShowValueText(valueCounter);
     }
 
     //四則演算子ボタンを押したとき
-    public void SetOperator(int operatorNo)
+    public void SetOperator(int no)
     {
-        if (!onFirstValue) return;//value2の入力後は受け付けない
-
-        operatorType = (Operator)System.Enum.ToObject(typeof(Operator), operatorNo);
-        onFirstValue = false;
-        value2 = new int[MAXdigit];
-        value2[0] = 0;
-        for(int i = 0; i < MAXdigit; i++)
+        if (valueCounter == 1)//計算処理
         {
-            value2[i] = -1;
+            Calculate();
         }
+
+        //2入力目の受付
+        operatorNo = no;
+        valueCounter++;
+        InitializeArray(valueCounter);
     }
 
     //計算処理
     public void Calculate()
     {
-        double v1 = ArrayToValue(value1, digitCounter1, dotPosition1);
-        double v2 = ArrayToValue(value2, digitCounter2, dotPosition2);
+        double v1 = ArrayToValue(0);
+        double v2 = ArrayToValue(1);
         double result = 0;
 
-        switch (operatorType)
+        switch (operatorNo)
         {
-            case Operator.Plus:
+            case 0://和
                 result = v1 + v2;
                 break;
-            case Operator.Minus:
+            case 1://差
                 result = v1 - v2;
                 break;
-            case Operator.Multiply:
+            case 2://積
                 result = v1 * v2;
                 break;
-            case Operator.Divide:
+            case 3://商
                 if (v2 == 0) break;//0で割るとエラーするので回避
 
                 result = v1 / v2;
                 break;
         }
 
-        //InitializeCalculation();
-
-        string s;
-        if (System.Math.Log10(result) > MAXdigit)
+        if (Math.Log10(result) > MAXdigit)
         {
-            s = "overFlow";
+            valueText.text = "overFlow";
+            InitializeCalculation();
         }
         else
         {
-            s = result.ToString();
+            valueCounter = 0;
+            ValueToArray(result, valueCounter);
+            ShowValueText(valueCounter);
+            AddPastValue(result);
         }
-        valueText.text = s;
-        value1 = ValueTextToArray(ref dotPosition1, ref digitCounter1);
     }
 
     public void SetDotPoint()
     {
-        if (onFirstValue)
+        if (digitCounters[valueCounter] == 0)
         {
-            if (digitCounter1 == 0)
-            {
-                dotPosition1 = 0;
-                digitCounter1++;
-            }
-            else
-            {
-                dotPosition1 = digitCounter1 - 1;
-            }
+            dotPositions[valueCounter] = 0;
+            digitCounters[valueCounter]++;
         }
         else
         {
-            if (digitCounter2 == 0)
-            {
-                dotPosition2 = 0;
-                digitCounter2++;
-            }
-            else
-            {
-                dotPosition2 = digitCounter2 - 1;
-            }
-        }
+            dotPositions[valueCounter] = digitCounters[valueCounter] - 1;
+        };
     }
 
-    public void Sine()
+    public void SinCosTan(int calcNo)
     {
-        double value;
+        double value = ArrayToValue(valueCounter);
+        value *= Math.PI / 180;
 
-        if (onFirstValue)
+        switch (calcNo)
         {
-            value = ArrayToValue(value1, digitCounter1, dotPosition1);
-            value *= System.Math.PI / 180;
-            value = System.Math.Sin(value);
-            valueText.text = value.ToString();
-            value1 = ValueTextToArray(ref dotPosition1, ref digitCounter1);
+            case 0://sine
+                value = Math.Sin(value);
+                break;
+            case 1://cosine
+                value = Math.Cos(value);
+                break;
+            case 2://tangent
+                value = Math.Tan(value);
+                break;
         }
-        else
+
+        ValueToArray(value, valueCounter);
+        ShowValueText(valueCounter);
+    }
+
+    public void ReferPastValue(bool up)
+    {
+        if (pastValues.Count == 0) return;
+
+        if ((up && pastIndex > 0) || pastIndex == pastValues.Count)
         {
-            value = ArrayToValue(value2, digitCounter2, dotPosition2);
-            value *= System.Math.PI / 180;
-            value = System.Math.Sin(value);
-            valueText.text = value.ToString();
-            value1 = ValueTextToArray(ref dotPosition2, ref digitCounter2);
+            pastIndex--;
         }
+        else if (!up && pastIndex < pastValues.Count - 1)
+        {
+            pastIndex++;
+        }
+
+        ValueToArray(pastValues[pastIndex], valueCounter);
+        ShowValueText(valueCounter);
     }
 }
 
